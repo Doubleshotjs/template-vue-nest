9a247ee3177369f3b998bbfc2aa817d74d7a0d6b
ArcherGu <649601376@qq.com>
2022-08-09 08:47:10 +0800

    feat: add useZoomFactor

--- packages/backend/preload.d.ts
+++ packages/backend/preload.d.ts
@@ -3,8 +3,7 @@ import type { IpcResponse } from '@doubleshot/nest-electron-ipc-transport'
 declare global {
   interface Window {
     electron: {
-      setZoomFactor(factor: number): void,
-      getDeviceScaleFactor(): Promise<IpcResponse<number>>
+      useZoomFactor(): { update: () => Promise<void> }
       saveImageToFile(image: string): Promise<IpcResponse<any>>
     },
     isElectron: boolean
--- packages/backend/src/preload.ts
+++ packages/backend/src/preload.ts
@@ -1,11 +1,32 @@
 import { contextBridge, webFrame, ipcRenderer } from 'electron'
 import { IpcResponse } from '@doubleshot/nest-electron-ipc-transport'
 
+function getDeviceScaleFactor(): Promise<IpcResponse<number>> {
+  return ipcRenderer.invoke("device-scale-factor")
+}
+
 contextBridge.exposeInMainWorld(
   'electron',
   {
-    setZoomFactor: webFrame.setZoomFactor.bind(webFrame),
-    getDeviceScaleFactor: (): Promise<IpcResponse<number>> => ipcRenderer.invoke("device-scale-factor"),
+    useZoomFactor: () => {
+      const DESIGN_HEIGHT = 1080
+      const DESIGN_DPR = 1
+      const DESIGN_SCALE_FACTOR = 1
+      let scaleFactor = 0
+
+      const update = async () => {
+        const height = window.innerHeight
+        const dpr = window.devicePixelRatio
+        if (scaleFactor === 0) scaleFactor = (await getDeviceScaleFactor()).data
+
+        const factor = (height / DESIGN_HEIGHT) * (dpr / DESIGN_DPR) * (DESIGN_SCALE_FACTOR / scaleFactor)
+        webFrame.setZoomFactor(factor)
+      }
+
+      return {
+        update
+      }
+    },
     saveImageToFile: (image: string): Promise<IpcResponse<any>> => ipcRenderer.invoke("save-image", image),
   },
 )
